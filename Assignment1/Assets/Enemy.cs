91b6e6bea715bd16f11484ae9df5a7c227cd7e6f
williamch1326 <williamch1326@gmail.com>
2020-11-10 08:51:26 -0800

    added midterm project

---
+++ Assignment1/Assets/Enemy.cs
@@ -0,0 +1,30 @@
+﻿
+using UnityEngine;
+
+public class Enemy : MonoBehaviour
+{
+    public float health = 50f;
+    public void takeDamage (float amount)
+    {
+        health -= amount;
+        if(health<=0f)
+        {
+            Dead();
+        }
+    }
+    void Dead()
+    {
+        Destroy(gameObject);
+    }
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
---
+++ Assignment1/Assets/Rifle.cs
@@ -0,0 +1,41 @@
+﻿
+using UnityEngine;
+
+public class Rifle : MonoBehaviour
+{
+    public float damage = 10f;
+    public float range = 100f;
+    public float fireRate = 15f;
+    public ParticleSystem muzzleFlash;
+
+    public Camera FPScamera;
+
+    private float nextTimeToFire = 0f;
+    // Update is called once per frame
+    void Update()
+    {
+        if(Input.GetButtonDown("Fire1"))
+        {
+            nextTimeToFire = Time.time + 1f / fireRate;
+            Shoot();
+        }
+    }
+
+    void Shoot()
+    {
+        muzzleFlash.Play();
+        RaycastHit hit;
+        if(Physics.Raycast(FPScamera.transform.position, FPScamera.transform.forward, out hit,range))
+        {
+            Debug.Log(hit.transform.name);
+             
+           Enemy enemy = hit.transform.GetComponent<Enemy>();
+
+            if(enemy!=null)
+            {
+                enemy.takeDamage(damage);
+            }
+               
+        }
+    }
+}
